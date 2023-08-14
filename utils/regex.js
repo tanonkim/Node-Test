e3e753ff15d69635fb06d47c36831ba41e51b2a4
tanonkim <robbins4bos@gmail.com>
2023-08-14 20:52:14 +0900

    Fix: Change exports

--- utils/regex.js
+++ utils/regex.js
@@ -1,5 +1,10 @@
-export const emailRegex =
+const emailRegex =
   /^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
-export const passwordRegex =
+const passwordRegex =
   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+module.exports = {
+  emailRegex,
+  passwordRegex,
+};
