d8be192c0ca68501a182dd91df8e9adc59467e9b
tanonkim <robbins4bos@gmail.com>
2023-08-14 20:15:38 +0900

    Feature: Add email && password regex

--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,24 @@
 const userDao = require("../models/userDao");
 
 const signUp = async (name, email, profileImage, password) => {
+  const emailRegex =
+    /^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+  if (!emailRegex.test(email)) {
+    const error = new Error("NOT_VALID_EMAIL_REGEX");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  if (!passwordRegex.test(password)) {
+    const error = new Error("NOT_VALID_PASSWORD_REGEX");
+    error.statusCode = 400;
+    throw error;
+  }
+
   return userDao.createUser(name, email, profileImage, password);
 };
 
