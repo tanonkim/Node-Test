e055653ad9d26fa099b98f930c707546e4146bda
tanonkim <robbins4bos@gmail.com>
2023-08-14 20:26:39 +0900

    Refactor: Add utils for regex

--- services/userService.js
+++ services/userService.js
@@ -1,12 +1,8 @@
 const userDao = require("../models/userDao");
 const bcrypt = require("bcrypt");
+const { emailRegex, passwordRegex } = require("../utils/regex");
+
 const signUp = async (name, email, profileImage, password) => {
-  const emailRegex =
-    /^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-
-  const passwordRegex =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-
   if (!emailRegex.test(email)) {
     const error = new Error("NOT_VALID_EMAIL_REGEX");
     error.statusCode = 400;
