df392b36acde16974e3c8cdd4d46aa2936e1a81b
tanonkim <robbins4bos@gmail.com>
2023-08-21 15:23:43 +0900

    Refactor: Change to be able to get data-result in success resonse

--- utils/baseResponse.js
+++ utils/baseResponse.js
@@ -1,14 +1,26 @@
+const baseResponseStatus = require("./baseResponseStatus");
 const CustomException = require("./handler/customException");
 
-const baseResponse = (err, req, res, next) => {
-  if (err instanceof CustomException) {
+const baseResponse = (dataOrError, res) => {
+  if (dataOrError instanceof CustomException) {
     return res.status(200).json({
-      isSuccess: err.isSuccess,
-      responseCode: err.responseCode,
-      responseMessage: err.responseMessage,
+      isSuccess: dataOrError.isSuccess,
+      responseCode: dataOrError.responseCode,
+      responseMessage: dataOrError.responseMessage,
     });
   }
 
+  // 데이터 반환 처리
+  if (dataOrError) {
+    return res.status(200).json({
+      isSuccess: baseResponseStatus.SUCCESS.isSuccess,
+      responseCode: baseResponseStatus.SUCCESS.responseCode,
+      responseMessage: baseResponseStatus.SUCCESS.responseMessage,
+      data: dataOrError,
+    });
+  }
+
+  // 그 외의 에러 처리
   return res.status(500).json({
     responseCode: 5000,
     message: "서버 내부 오류",
