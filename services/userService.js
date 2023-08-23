023cc5e23a68b3dded12b9185292d93496b29372
tanonkim <robbins4bos@gmail.com>
2023-08-23 18:04:24 +0900

    Feature: Add updateContent in userService

--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,5 @@
 const userDao = require("../models/userDao");
+const postDao = require("../models/postDao");
 const bcrypt = require("bcrypt");
 const { emailRegex, passwordRegex } = require("../utils/regex");
 const jwt = require("jsonwebtoken");
@@ -10,6 +11,7 @@ const {
   NONE_POST,
 } = require("../utils/baseResponseStatus");
 const CustomException = require("../utils/handler/customException");
+const { deleteImageFromS3, uploadToS3 } = require("../utils/s3/imageUploader");
 const secretKey = process.env.SECRET_KEY;
 
 const signUp = async (name, email, profileImage, password) => {
@@ -78,7 +80,7 @@ const processPosts = (rows) => {
   return [...postMap.values()];
 };
 
-const updatePostContent = async (userId, postId, content) => {
+const updatePostContent = async (userId, postId, content, updatedUrls) => {
   try {
     const post = await userDao.getPostByIdAndUserId(userId, postId);
 
@@ -86,7 +88,20 @@ const updatePostContent = async (userId, postId, content) => {
       throw new CustomException(NONE_POST);
     }
 
+    if (updatedUrls.length) {
+      const existingImages = await userDao.getImagesByPostId(postId); // 해당 포스트의 기존 이미지 URL 가져오기
+      for (let image of existingImages) {
+        await deleteImageFromS3(image.post_image_url); // S3에서 기존 이미지를 삭제
+        await userDao.deleteImageRecord(image.id); // PostImage 테이블에서 기존 이미지 레코드를 삭제
+      }
+
+      // DB 업데이트
+      for (const imageUrl of updatedUrls) {
+        await postDao.saveImageUrl(postId, imageUrl, userId);
+      }
+    }
     await userDao.updatePostContent(userId, postId, content);
+
     return await userDao.getUpdatedPost(userId, postId);
   } catch (error) {
     console.log(error);
